e0141d539295be27e5edfb02fc931b1b32a485bf
Miguel Sarmiento <mangelsar@gmail.com>
2026-05-20 17:08:43 -0500

    chore: eliminar import flutter/services.dart no utilizado

--- lib/screens/create_product_screen.dart
+++ lib/screens/create_product_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
